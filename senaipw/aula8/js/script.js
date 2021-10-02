726db09f139962c05dca418cf3849e826f188203
Frklcedo <franklinmacedo5115@gmail.com>
2021-10-02 14:35:02 -0300

    Primeiros passos javascript senai

---
+++ senaipw/aula8/js/script.js
@@ -0,0 +1,17 @@
+alert('Ola, JS');
+
+var conf = confirm('É humano?');
+if(conf){
+    alert('Não é não >:[');
+}
+else{
+    alert('Não é mesmo');
+}
+
+var horas = prompt('Quantas horas estudou hoje?','1?');
+if(parseInt(horas)>=2){
+    alert('Boaaaa');
+}
+else{
+    alert('Estude mais');
+}
